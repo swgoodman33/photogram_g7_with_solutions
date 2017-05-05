752889c88a16ac44b30190558fdac341e3d47a06
swgoodman33 <swgoodman33@users.noreply.github.com>
2017-05-04 20:02:34 -0500

    Almost done

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,26 +4,59 @@ class PicturesController < ApplicationController
   end
 
   def create_row
-    render("pic_templates/create_row.html.erb")
+
+    p = Photo.new
+
+    p.caption = params["the_caption"]
+    p.source = params["the_source"]
+    p.save
+    @current_count = Photo.count
+
+    redirect_to("/photos/")
   end
 
   def index
+    @list_of_photos = Photo.all.order(:created_at => :desc)
     render("pic_templates/index.html.erb")
   end
 
   def show
+    # The paramaters look like: {"the_id"=> "4"}
+
+    @my_photo = Photo.find(params["the_id"])
+
+    id_number = params["the_id"]
+
+    p = Photo.find(id_number)
+
+    @the_caption = p.caption
+    @the_source = p.source
+    @the_timestamp = p.created_at
     render("pic_templates/show.html.erb")
   end
 
   def edit_form
+    @my_photo = Photo.find(params["the_id"])
+
     render("pic_templates/edit_form.html.erb")
   end
 
   def update_row
-    render("pic_templates/update_row.html.erb")
+    id_number = params["the_id"]
+    p = Photo.find(id_number)
+    p.source = params["the_source"]
+    p.caption = params["the_caption"]
+    p.save
+
+    redirect_to("/photos/#{p.id}")
   end
 
   def destroy_row
-    render("pic_templates/destroy_row.html.erb")
+    p = Photo.find(params["the_id"])
+    p.destroy
+
+    @current_count = Photo.count
+
+    redirect_to("/photos/")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Rails.application.routes.draw do
   # READ
   get("/photos", { :controller => "pictures", :action => "index" })
   get("/photos/:the_id", { :controller => "pictures", :action => "show" })
+  get("/", { :controller => "pictures", :action => "index" })
 
   # UPDATE
-  get("/photos/:la_id/edit", { :controller => "pictures", :action => "edit_form" })
-  get("/update_photo/:le_id", { :controller => "pictures", :action => "update_row" })
+  get("/photos/:the_id/edit", { :controller => "pictures", :action => "edit_form" })
+  get("/update_photo/:the_id", { :controller => "pictures", :action => "update_row" })
 
   # DELETE
-  get("/delete_photo/:da_id", { :controller => "pictures", :action => "destroy_row" })
+  get("/delete_photo/:the_id", { :controller => "pictures", :action => "destroy_row" })
 end
